d7488c3a48cc0e71d5ce36f656d120a811922f2f
Peter Parente <parente@cs.unc.edu>
2010-04-09 20:12:36 -0400

    First commit
    * Started TicTacToe widget
    * Coded together during 4/9 meetup

---
+++ tictactoe.js
@@ -0,0 +1,10 @@
+/**
+ * Startup script for TicTacToe. Nothing to see here yet.
+ *
+ * Copyright UNC Open Web Team 2010. All Rights Reserved.
+ */
+dojo.require('widgets.TicTacToe');
+
+dojo.ready(function() {
+    
+});
